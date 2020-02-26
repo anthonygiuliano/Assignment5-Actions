1d65ffd494bd257736b90109d43b6f05bf5d4970
Anthony Giuliano <agiuliano@gmail.com>
2020-02-25 19:59:38 -0800

    fixes whitespace

--- tests.py
+++ tests.py
@@ -7,7 +7,6 @@ class TestCase(unittest.TestCase):
         expected = 'success'
         self.assertEqual(expected, task.firstrun())
 
-
     def test2(self):
         expected = 'failure'
         self.assertEqual(expected, task.firstrun())
